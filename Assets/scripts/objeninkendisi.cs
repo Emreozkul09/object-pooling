081cf552c9687c25c8cb24e209ffc5d8a8c50532
Emre Özkul <ozkul9018@gmail.com>
2024-02-14 22:01:09 +0300

    object pooling
    simple object pooling example

---
+++ Assets/scripts/objeninkendisi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objeninkendisi : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pasiflestir")) 
+        {
+            gameObject.SetActive(false);
+
+        }
+    }
+
+}
---
+++ Assets/scripts/oyunkontrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyunkontrol : MonoBehaviour
+{
+    public GameObject[] Olusacakobje;
+    public GameObject OlusacakNokta;
+    int sira_numarasi=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       /* Olusacakobje[0].SetActive(true);
+        Olusacakobje[0].transform.position = OlusacakNokta.transform.position;*/
+               StartCoroutine(objeolustur());
+    }
+
+   
+
+    IEnumerator objeolustur()
+    {
+
+        while(true)
+        {
+
+            if (sira_numarasi<=8)
+            {
+                Olusacakobje[sira_numarasi].SetActive(true);
+                Olusacakobje[sira_numarasi].transform.position = OlusacakNokta.transform.position;
+
+               
+                sira_numarasi++;
+
+            }
+            else
+            {
+                sira_numarasi = 0;
+                Olusacakobje[sira_numarasi].SetActive(true);
+                Olusacakobje[sira_numarasi].transform.position = OlusacakNokta.transform.position;
+            }
+           
+            yield return new WaitForSeconds(.5f);
+
+        }
+       
+
+    }
+}
